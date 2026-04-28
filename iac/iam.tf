29d2bd6aee4f7c56243c96d48ab6c29e1f5cd468
Ranvijay <2472526@cognizant.com>
2026-04-28 09:12:04 +0530

    fix ec2 iam permission

--- iac/iam.tf
+++ iac/iam.tf
@@ -19,11 +19,16 @@ resource "aws_iam_instance_profile" "ec2_profile" {
  name = "ec2-profile"
  role = aws_iam_role.main.name
 }
-resource "aws_iam_role_policy_attachment" "ecs_exec" {
+resource "aws_iam_role_policy_attachement" "ec2_full" {  #ec2
+  role = aws_iam_role.main.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_exec" {   #ecs
  role       = aws_iam_role.main.name
  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
-resource "aws_iam_role_policy_attachment" "step_fn" {
+resource "aws_iam_role_policy_attachment" "step_fn" {   # step_function
  role       = aws_iam_role.main.name
  policy_arn = "arn:aws:iam::aws:policy/AWSStepFunctionsFullAccess"
 }
