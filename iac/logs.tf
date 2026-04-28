5e7d5928374213e60838385319fc14da8dd3f9cb
Ranvijay <2472526@cognizant.com>
2026-04-28 14:40:55 +0530

    clean stepfunction logging

--- iac/logs.tf
+++ iac/logs.tf
@@ -1,6 +1,6 @@
-resource "aws_cloudwatch_log_group" "main" {
-  name = "/aws/stepfunctions/final-project"
-}
+# resource "aws_cloudwatch_log_group" "main" {
+ # name = "/aws/stepfunctions/final-project"
+#}
 
 resource "aws_cloudwatch_log_group" "ecs" {
   name = "/aws/ecs/final-project"
