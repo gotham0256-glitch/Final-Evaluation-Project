0c34a8ca4bf7b06fc9906b7d359e8e3ddd8dd4dd
Ranvijay <2472526@cognizant.com>
2026-05-11 23:49:09 +0530

    final update

--- iac/iam.tf
+++ iac/iam.tf
@@ -1,4 +1,4 @@
-#This defines who is allowed to assume (use) the role.
+#This defines who is allowed to assume (use) permission/trust the role.
 data "aws_iam_policy_document" "assume_role" {
  statement {
   #This means services can “take” this role temporarily.
@@ -8,7 +8,10 @@ data "aws_iam_policy_document" "assume_role" {
      identifiers = [
        "ec2.amazonaws.com",
        "ecs-tasks.amazonaws.com",
-       "states.amazonaws.com"
+       "states.amazonaws.com",
+       #
+        "events.amazonaws.com"
+
      ]
    }
  }
@@ -19,23 +22,25 @@ resource "aws_iam_role" "main" {
  #Attaches the trust policy
  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
-resource "aws_iam_instance_profile" "ec2_profile" {
+resource "aws_iam_instance_profile" "ec2_profile" { #creates an instance profile for EC2 to use the IAM role
  name = "ec2-profile"
  role = aws_iam_role.main.name
 }
 
-resource "aws_iam_role_policy_attachment" "policies" {
+resource "aws_iam_role_policy_attachment" "policies" {  #
  for_each = {
    ec2_full    = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
    ecs_exec    = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
    step_fn     = "arn:aws:iam::aws:policy/AWSStepFunctionsFullAccess"
    eventbridge = "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
+   #
+   cloudwatchlog = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
  }
- role       = aws_iam_role.main.name
- policy_arn = each.value
+ role       = aws_iam_role.main.name #kis role pe attach karna hai policies, yeh value loop ke bahar se aayegi, kyunki sab policies same role pe attach karni hai
+ policy_arn = each.value  #kis policy ko attach karna hai role pe, yeh value each loop se aayegi
 }
 
-resource "aws_iam_role_policy" "stepfunction_logs" {
+/* resource "aws_iam_role_policy" "stepfunction_logs" {
   role = aws_iam_role.main.id
   policy = jsonencode({
     Version = "2012-10-17"
@@ -78,3 +83,4 @@ resource "aws_iam_role_policy" "eventbridge_policy" {
    }]
  })
 }
+ */
